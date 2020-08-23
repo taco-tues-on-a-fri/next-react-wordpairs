59049538f3668b9c5ff6839c2b1f869da85cc8ac
Mike Jadick <mike@jadick.dev>
2020-08-23 17:06:03 -0600

    Add contextVerbsNouns

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -3,6 +3,7 @@ import adjectiveJson from '../../static/word-data/adjectives.json'
 import adverbJson from '../../static/word-data/adverbs.json'
 import nounJson from '../../static/word-data/nouns.json'
 import contextSettingsJson from '../../static/word-data/settings-adjective-noun-location.json'
+import contextVerbsNounsJson from '../../static/word-data/context-verbs-applied-nouns.json'
 import prepositionJson from '../../static/word-data/prepositions.json'
 
 type Data = {
@@ -10,6 +11,7 @@ type Data = {
   adverbs: string[] 
   nouns: string[]
   contextSettings: string[]
+  contextVerbsAppliedNouns: string[]
   prepositions: string[]
 }
 
@@ -21,6 +23,7 @@ export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
     nouns: nounJson.nouns,
     contextSettings: contextSettingsJson.settings_adjective_noun_location,
     prepositions: prepositionJson.prepositions,
+    contextVerbsAppliedNouns: contextVerbsNounsJson.verbs_applied_nouns,
   })
 }
 
