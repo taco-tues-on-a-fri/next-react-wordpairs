1ba0bdcdec8bf1926b5eb4a078d3974580c34eb1
Mike Jadick <mike@jadick.dev>
2020-08-23 16:40:51 -0600

    Create nouns.json

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -1,14 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import adjectiveJson from '../../static/word-data/adjectives.json'
+import adverbJson from '../../static/word-data/adverbs.json'
+import nounJson from '../../static/word-data/nouns.json'
 
 type Data = {
   adjectives: string[] 
+  adverbs: string[] 
+  nouns: string[] 
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
   res.statusCode = 200
   res.json({ 
-    adjectives: adjectiveJson.adjectives
+    adjectives: adjectiveJson.adjectives,
+    adverbs: adverbJson.adverbs,
+    nouns: nounJson.nouns,
   })
 }
 
