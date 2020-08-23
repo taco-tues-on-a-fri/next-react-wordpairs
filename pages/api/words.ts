0826425a2a0e2514c63014e8f7089f5453889be4
Mike Jadick <mike@jadick.dev>
2020-08-23 16:48:05 -0600

    Add contextSettings

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -2,11 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import adjectiveJson from '../../static/word-data/adjectives.json'
 import adverbJson from '../../static/word-data/adverbs.json'
 import nounJson from '../../static/word-data/nouns.json'
+import contextSettingsJson from '../../static/word-data/settings-adjective-noun-location.json'
 
 type Data = {
   adjectives: string[] 
   adverbs: string[] 
-  nouns: string[] 
+  nouns: string[]
+  contextSettings: string[]
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
@@ -15,6 +17,7 @@ export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
     adjectives: adjectiveJson.adjectives,
     adverbs: adverbJson.adverbs,
     nouns: nounJson.nouns,
+    contextSettings: contextSettingsJson.settings_adjective_noun_location,
   })
 }
 
