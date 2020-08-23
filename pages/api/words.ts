ce76eb72e92a7ce95d86369e19210726a73bfb34
Mike Jadick <mike@jadick.dev>
2020-08-23 17:08:59 -0600

    Add verbsJson

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -1,18 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import adjectiveJson from '../../static/word-data/adjectives.json'
 import adverbJson from '../../static/word-data/adverbs.json'
-import nounJson from '../../static/word-data/nouns.json'
 import contextSettingsJson from '../../static/word-data/settings-adjective-noun-location.json'
 import contextVerbsNounsJson from '../../static/word-data/context-verbs-applied-nouns.json'
+import nounJson from '../../static/word-data/nouns.json'
 import prepositionJson from '../../static/word-data/prepositions.json'
+import verbJson from '../../static/word-data/verbs.json'
 
 type Data = {
-  adjectives: string[] 
-  adverbs: string[] 
-  nouns: string[]
-  contextSettings: string[]
-  contextVerbsAppliedNouns: string[]
-  prepositions: string[]
+  adjectives: string[];
+  adverbs: string[];
+  contextSettings: string[];
+  contextVerbsAppliedNouns: string[];
+  nouns: string[];
+  prepositions: string[];
+  verbs: string[];
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
@@ -20,10 +22,10 @@ export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
   res.json({ 
     adjectives: adjectiveJson.adjectives,
     adverbs: adverbJson.adverbs,
-    nouns: nounJson.nouns,
     contextSettings: contextSettingsJson.settings_adjective_noun_location,
-    prepositions: prepositionJson.prepositions,
     contextVerbsAppliedNouns: contextVerbsNounsJson.verbs_applied_nouns,
+    nouns: nounJson.nouns,
+    prepositions: prepositionJson.prepositions,
+    verbs: verbJson.verbs,
   })
 }
-
