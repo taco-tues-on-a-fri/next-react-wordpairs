c8fb943cc5c4dbea6f7b379152b6d001c4751247
Mike Jadick <mike@jadick.dev>
2020-08-23 16:50:43 -0600

    Add prepositionsJson

--- pages/api/words.ts
+++ pages/api/words.ts
@@ -3,12 +3,14 @@ import adjectiveJson from '../../static/word-data/adjectives.json'
 import adverbJson from '../../static/word-data/adverbs.json'
 import nounJson from '../../static/word-data/nouns.json'
 import contextSettingsJson from '../../static/word-data/settings-adjective-noun-location.json'
+import prepositionJson from '../../static/word-data/prepositions.json'
 
 type Data = {
   adjectives: string[] 
   adverbs: string[] 
   nouns: string[]
   contextSettings: string[]
+  prepositions: string[]
 }
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
@@ -18,6 +20,7 @@ export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
     adverbs: adverbJson.adverbs,
     nouns: nounJson.nouns,
     contextSettings: contextSettingsJson.settings_adjective_noun_location,
+    prepositions: prepositionJson.prepositions,
   })
 }
 
